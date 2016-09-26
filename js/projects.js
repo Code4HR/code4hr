e646cd8557b7301aed1dc03ab828f851bf0a91cf
Stanley Zheng <dev.stanzheng@gmail.com>
2016-09-25 22:36:07 -0400

    fix project page

--- js/projects.js
+++ js/projects.js
@@ -1,4 +1,4 @@
-var cfapi_url = 'http://codeforamerica.org/api/organizations/Code-for-Hampton-Roads/projects';
+var cfapi_url = 'http://codeforamerica-api.herokuapp.com/api/organizations/Code-for-Hampton-Roads/projects';
 
 // Go get projects! Then show them off.
 $.getJSON(cfapi_url, showProjects);
